93a1028ff40fdf43a386ff90a25daaf2194335bf
MITCHELL CHILDERHOSE <mn209073@DDSB.CA>
2020-04-24 10:02:57 -0400

    Scripts renamed

--- On-Marg Compilation.R
+++ On-Marg Compilation.R
@@ -1,13 +1,18 @@
-#This file constructs the ON-MARG version of the SRI using geocoded student
-#files and On-Marg summaries attached to each DA.
+#This file constructs the ON-MARG data and merges it with
+#Statistics Canada DA Boundary files for mapping.
+
 # Getting Started with On-Marg data ---------------------------------------
 library(httr)       #Access and download the online file
 library(XLConnect)  #Load data from the downloaded excel file
-library(sf)
-#Download the OnMarg datafile - working with Worksheet 4 with DA data
+library(sf)         #Managing ESRI Shape files and geometry
+library(viridis)    #Color pallette
+
+##########Download the OnMarg datafile - working with Worksheet 4 with DA data
 GET("https://www.publichealthontario.ca/-/media/data-files/index-on-marg.xls?la=en", 
     write_disk(tf <- tempfile(fileext = ".xls")))  #save as temporary file
+
 wb <- loadWorkbook(tf)           #load the excel file
+
 #Excel File layout:
 # Worksheet 1: 2016 ON-Marg: file description
 # Worksheet 2: Variable Descriptions
@@ -22,36 +27,45 @@ wb <- loadWorkbook(tf)           #load the excel file
 # Worksheet 11: 2016_PHUUID
 # Worksheet 12: 2016_LHINUID
 # Worksheet 13: 2016_LHINSRUID
+
 # Load in Worksheet
 OnMarg.raw <- readWorksheet(wb, sheet=4) #load the data from the
-#Loading Ontario DA Shape File
-GET("http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/2016/lda_000a16a_e.zip", 
-    write_disk(DAtemp <- tempfile(fileext = ".zip")))  #save as temporary file
-#unzipping DA file
-DA.zip <- read.table(unz("Sales.zip", "Sales.dat"), nrows=10, header=T, quote="\"", sep=",")
-#extracting the shape file
-DA.zip <- st_read(unz(DAtemp, "lda_000a16a_e.shp"), stringsAsFactors = FALSE)
-#Download the Statistics Canada DA Boundary Shape file
+
+##########Download the Statistics Canada DA Boundary Shape file
 #create temporary files
 temp <- tempfile()
 temp2 <- tempfile()
+
 #download the zip folder from the internet save to 'temp' 
 #this is a large file and will take a while
 download.file("http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/2016/lda_000a16a_e.zip",temp)
+
 #unzip the contents in 'temp' and save unzipped content in 'temp2'
 unzip(zipfile = temp, exdir = temp2)
+
 #finds the filepath of the shapefile (.shp) file in the temp2 unzip folder
 #the $ at the end of ".shp$" ensures you are not also finding files such as .shp.xml 
 DA_SHP_file<-list.files(temp2, pattern = ".shp$",full.names=TRUE)
+
 #Load the DA shape file
 DA <- st_read(DA_SHP_file, stringsAsFactors = FALSE)
+
 #decide on a CRS that you want to use:  
 CRStoUse <- st_crs("+init=EPSG:4269")  #NAD83 UTM zone 17N
 DA.reprojected <- st_transform(DA, CRStoUse)
+
 #align the CRS
-OnMargDA <- merge(DA.reprojected, OnMarg.raw, by = "DAUID")
+OnMargDA <- merge(DA.reprojected, OnMarg.raw, by = "DAUID") %>%
+  mutate(Summary = (Instability_q_DA16 +
+                    Deprivation_q_DA16 +
+                    Dependency_q_DA16 +
+                    Ethniccon_q_DA16)/4)
+
 #plot the provincial DA shape file
-ggplot() +
-  geom_sf(data=OnMargDA) +
+
+ggplot(data = OnMargDA, aes(fill=Summary, color=Summary)) +
+  geom_sf() +
+  scale_fill_viridis() + 
+  scale_color_viridis() +
   ggtitle("OnMarg Data") +
   coord_sf()
--- OnMarg Index Compilation.R
+++ OnMarg Index Compilation.R
@@ -1,44 +1,60 @@
-# Getting Started with On-Marg data ---------------------------------------
-library(httr)       #Access and download the online file
-library(XLConnect)  #Load data from the downloaded excel file
+#This file uses the merged On-Marg Stats Canada Boundary
+#to create an index using a point file.
 
-# Creating a School Index with On-Marg Data -------------------------------
+# Creating an Index with On-Marg Data -------------------------------
+
 #The following code requires a DA shape file (you can use the one created above)
-#and a student geocoded point shape file (which you should be able to get from 
-#your Planning Dept.)
+#and a geocoded point shape file  
+
 library(sf)
+
+#file locations
+Shapefile <- "C:/GIS/OnMarg - 1920 Geo/DDSB_DA_ONMarg16_UTMNAD81Z17N.shp"
+Pointfile <- "C:/GIS/Points.shp"
+
+
 #loading shape data  (here I have used my own)
-OnMarg <- st_read("C:/GIS/OnMarg - 1920 Geo/DDSB_DA_ONMarg16_UTMNAD81Z17N.shp", stringsAsFactors = FALSE)
-Geocode <- st_read("C:/GIS/OnMarg - 1920 Geo/Student_Detail_2019_10_All.shp", stringsAsFactors = FALSE)
+OnMarg <- st_read(Shapefile, stringsAsFactors = FALSE)
+Geocode <- st_read(Pointfile, stringsAsFactors = FALSE)
+
 #aligning CRS
 #Check to see whether the CRS types are the same
 st_crs(OnMarg)
 st_crs(Geocode)
+
 #decide on a CRS that you want to use:  
 CRStoUse <- st_crs("+init=EPSG:4269")  #NAD83 UTM zone 17N
 Geocode.reprojected <- st_transform(Geocode, CRStoUse)
 OnMarg.reprojected <- st_transform(OnMarg, CRStoUse)
+
 #####View shapefile metadata
 #geometry type
 st_geometry_type(OnMarg)
+
 #boundary extent
 st_bbox(OnMarg)
+
 #####plot the map
 ggplot() +
   geom_sf(data=OnMarg.reprojected) +
   geom_sf(data=Geocode.reprojected) +
   ggtitle("DDSB DAs and OnMarg Data") +
   coord_sf()
+
 ##spatial join - add the attributes of the polygons (OnMarg.reprojected) 
 ##to the geocoded points (Geocode.reprojected)
 ##First make sure both objects are using the same CRS
 SJtest <- st_join(Geocode.reprojected, left = FALSE, OnMarg.reprojected)
+
 #School Summaries
+
 SchoolSummary <- SJtest %>%
   group_by(Schoolname, SchoolCode) %>%
   summarize(OnMargIndex = mean(as.numeric(Summary)))
+
 #check to see what the spatially joined data looks like
 SchoolSummary
+
 #check to see what the spatially joined object looks like plotted
 #note that although there are only school records, every individual
 #student point appears to be retained in the geometry
@@ -46,5 +62,7 @@ ggplot() +
   geom_sf(data=SchoolSummary) +
   ggtitle("School Summary and OnMarg Data") +
   coord_sf()
+
 #Convert the spatially joined shape object to a dataframe
 SchoolSummary.df <- st_drop_geometry(SchoolSummary)
+
